cc3f9b57a7d2e1a666f2352439e33259eeb8d585
Davi Torelli <davitorelli@outlook.com>
2025-04-07 19:05:20 -0300

    Routing the application

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
+//* Libraries imports
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "@/pages/App";
+import { BrowserRouter } from "react-router-dom";
+
+//* Local imports
 import "@/index.css";
+import App from "./pages/App";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
